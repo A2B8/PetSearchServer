e1e7c8e55689d4234d69faf0729f934f8ba0a0d3
A2B8 <92379746+A2B8@users.noreply.github.com>
2023-01-25 17:45:27 +0500

    Delete Pet_Search directory

--- Pet_Search/src/main/java/com/example/Pet_Search/registration/RegistrationService.java
+++
@@ -1,7 +0,0 @@
-package com.example.Pet_Search.registration;
-
-public class RegistrationService {
-    public String register(RegistrationRequest request) {
-        return "Оно живое!";
-    }
-}
--- Pet_Search/src/main/java/com/example/Pet_Search/user/UserRole.java
+++
@@ -1,6 +0,0 @@
-package com.example.Pet_Search.user;
-
-public enum UserRole {
-    USER,
-    ADMIN
-}
