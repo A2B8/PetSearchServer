7e67cb27e71c6a7d6448c19f600b883fab99caa2
A2B8 <92379746+A2B8@users.noreply.github.com>
2023-01-25 17:45:36 +0500

    Delete SFP directory

--- SFP/src/main/java/com/project/SFP/email/EmailSender.java
+++
@@ -1,5 +0,0 @@
-package com.project.SFP.email;
-
-public interface EmailSender {
-    void send(String to, String email);
-}
--- SFP/src/main/java/com/project/SFP/user/UserRole.java
+++
@@ -1,6 +0,0 @@
-package com.project.SFP.user;
-
-public enum UserRole {
-    USER,
-    ADMIN
-}
